76b40d248270992f26fc857f02bdbb55f56756fe
Arina <crowlynx4@gmail.com>
2023-03-05 20:52:49 +0200

    commit

---
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -0,0 +1,35 @@
+package taxi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "YOURS URL";
+    private static final String USERNAME = "YOURS USERNAME";
+    private static final String PASSWORD = "YOURS PASSWORD";
+    private static final String JDBC_DRIVER = "YOURS JDBC_DRIVER";
+
+    private ConnectionUtil() {
+    }
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
+    }
+}
